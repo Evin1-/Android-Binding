7c51e327d44c3f0e1c9ce17ff6d9cc3825977728
Evin1- <edwin.hdz.12.0@gmail.com>
2016-09-05 20:15:24 -0400

    Add EventListener

---
+++ app/src/main/java/com/example/bindingtest/EventListener.java
@@ -0,0 +1,15 @@
+package com.example.bindingtest;
+
+import android.util.Log;
+import android.view.View;
+
+/**
+ * Created by evin on 9/5/16.
+ */
+public class EventListener {
+    private static final String TAG = "MyHandlersTAG_";
+
+    public void onClickFriend(View view) {
+        Log.d(TAG, "onClickFriend: ");
+    }
+}
